9c5c96fd77185ade045ee3d71d9c3a6d36b4715f
Nuno Marques <ositaka@gmail.com>
2024-02-01 23:43:21 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,51 +81,51 @@ const config = {
           },
         ],
       },
-      footer: {
-        style: 'dark',
-        // links: [
-        //   {
-        //     title: 'Docs',
-        //     items: [
-        //       {
-        //         label: 'Tutorial',
-        //         to: '/docs/intro',
-        //       },
-        //     ],
-        //   },
-        //   {
-        //     title: 'Community',
-        //     items: [
-        //       {
-        //         label: 'Stack Overflow',
-        //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-        //       },
-        //       {
-        //         label: 'Discord',
-        //         href: 'https://discordapp.com/invite/docusaurus',
-        //       },
-        //       {
-        //         label: 'Twitter',
-        //         href: 'https://twitter.com/docusaurus',
-        //       },
-        //     ],
-        //   },
-        //   {
-        //     title: 'More',
-        //     items: [
-        //       {
-        //         label: 'Blog',
-        //         to: '/blog',
-        //       },
-        //       {
-        //         label: 'GitHub',
-        //         href: 'https://github.com/ositaka/learn-dutch',
-        //       },
-        //     ],
-        //   },
-        // ],
-        copyright: `A project by Nuno Marques, built with Docusaurus.`,
-      },
+      // footer: {
+      //   style: 'dark',
+      //   // links: [
+      //   //   {
+      //   //     title: 'Docs',
+      //   //     items: [
+      //   //       {
+      //   //         label: 'Tutorial',
+      //   //         to: '/docs/intro',
+      //   //       },
+      //   //     ],
+      //   //   },
+      //   //   {
+      //   //     title: 'Community',
+      //   //     items: [
+      //   //       {
+      //   //         label: 'Stack Overflow',
+      //   //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+      //   //       },
+      //   //       {
+      //   //         label: 'Discord',
+      //   //         href: 'https://discordapp.com/invite/docusaurus',
+      //   //       },
+      //   //       {
+      //   //         label: 'Twitter',
+      //   //         href: 'https://twitter.com/docusaurus',
+      //   //       },
+      //   //     ],
+      //   //   },
+      //   //   {
+      //   //     title: 'More',
+      //   //     items: [
+      //   //       {
+      //   //         label: 'Blog',
+      //   //         to: '/blog',
+      //   //       },
+      //   //       {
+      //   //         label: 'GitHub',
+      //   //         href: 'https://github.com/ositaka/learn-dutch',
+      //   //       },
+      //   //     ],
+      //   //   },
+      //   // ],
+      //   copyright: `A project by Nuno Marques, built with Docusaurus.`,
+      // },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
